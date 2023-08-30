fb965ab604afcc0df054410e0d65d7fe02d6e1bd
Soledadkesi <soledadkesi1995@gmail.com>
2023-08-30 11:39:31 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - that prints a string, followed by a new line
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		putchar('\n');
+
+}
