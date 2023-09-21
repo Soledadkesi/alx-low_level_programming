b7f35715861125a0d22032aa892aa5d1bb27fedd
Soledadkesi <soledadkesi1995@gmail.com>
2023-09-21 14:56:40 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,6 +4,7 @@
 /**
 * print_list - prints all the elements of a list_t list
 * @h: pointer to the head of the list
+*
 * Return: the number of nodes
 */
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,21 +1,26 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-* struct list_s - singly linked list
-* @str: string 
-* @len: length of the string
-* @next: points to the next node
-* Description: singly linked list node structure
-*/
-
+ * struct list_s - singly linked list
+ * @str: string 
+ * @len: length of the string
+ * @next: shows to the next node
+ *
+ * Description:  linked list node structure
+ * for Alx-SE  project
+ */
 typedef struct list_s
-
 {
 	char *str;
-	int len;
+	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
