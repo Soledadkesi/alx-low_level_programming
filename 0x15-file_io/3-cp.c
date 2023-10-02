9efc073ac71d21a78a85f9d423c9dbf0cc48f17e
Soledadkesi <soledadkesi1995@gmail.com>
2023-10-02 10:35:38 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,7 +47,7 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
- *  main - Copies the contents of a file to another file.Description: If the argument count is incorrect - exit code 97.
+ * Description: If the argument is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
