369e73dcf0a86f6b5e5efaefbd4c24fb120b86da
Soledadkesi <soledadkesi1995@gmail.com>
2023-11-15 12:37:53 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp/ https://github.com/sole-eng/alx-low_level_programming/raw/master/0x18-dynamic_libraries/mymole.so
-export LD_PRELOAD="/tmp/mymole.so"
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
